f32b28f23b246a585dd7ad9cb8094d3624ab9817
k-yeonjin <rlfduswls@gmail.com>
2023-07-01 00:02:26 +0900

    07.01

---
+++ order1.js
@@ -0,0 +1,12 @@
+const headerA=document.querySelector("#headerA");
+const headerB=document.querySelector("#headerB");
+headerA.addEventListener("click",(e)=>{
+    headerA.style.backgroundColor="#FFBCBC";
+    headerB.style.backgroundColor="#E6E6E6";;
+   })
+
+   headerB.addEventListener("click",(e)=>{
+    headerB.style.backgroundColor="#FFBCBC";
+    headerA.style.backgroundColor="#E6E6E6";;
+   })   
+
---
+++ order2.js
@@ -0,0 +1,17 @@
+const headerA=document.querySelector("#headerA");
+const headerB=document.querySelector("#headerB");
+headerA.addEventListener("click",(e)=>{
+    headerA.style.backgroundColor="#FFBCBC";
+    headerB.style.backgroundColor="#E6E6E6";;
+   })
+
+   headerB.addEventListener("click",(e)=>{
+    headerB.style.backgroundColor="#FFBCBC";
+    headerA.style.backgroundColor="#E6E6E6";;
+   })   
+
+   const ggim=document.querySelector("#ggim i");
+   
+   function ggimchange(){
+    ggim.setAttribute('class',"fa-thin fa-heart")}
+

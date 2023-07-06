b7e00caa0da1e9250def986e1b05a16fd9c6be82
k-yeonjin <rlfduswls@gmail.com>
2023-07-06 09:17:22 +0900

    070

--- menu.js
+++ menu.js
@@ -1,573 +1,61 @@
+var total = 0;
+var totalElement = document.getElementById("total");
 
-var totalnum=0;
-function totalnums(){
-  totalnum=0;
-  document.getElementById("total").innerText=totalnum;
-}
-var count1=1;
+function handleMenuClick(menu) {
+  var numButton = menu.querySelector("#numbutton");
 
-var count1A=function(){
- 
-  if(count1>1){
-    count1=count1-1;
-    totalnum=totalnum-1;
-  }
-  
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result1").innerText=count1;
-
-}
-
-function count1B(){
-  count1=count1+1;
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result1").innerText=count1;
-  document.getElementById("total").innerText=totalnum;
- 
-}
-
-function resetcount1(){
-  count1=1;
-  document.getElementById("result1").innerText=count1;
-  
-}
-
-
-var count2=1;
-var count2A=function(){
-  if(count2>1){
-    count2=count2-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result2").innerText=count2;
-}
-
-var count2B=function(){
-
-  count2=count2+1;
-  totalnum=totalnum+1
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result2").innerText=count2;
-}
-
-function resetcount2(){
-  count2=1;
-  document.getElementById("result2").innerText=count2;
-
-}
-
-var count3=1;
-var count3A=function(){
-
-  if(count3>1){
-    count3=count3-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result3").innerText=count3;
-}
-
-var count3B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count3=count3+1;
-  document.getElementById("result3").innerText=count3;
-}
-
-function resetcount3(){
-  count3=1;
-  document.getElementById("result3").innerText=count3;
-
-}
-
-var count4=1;
-var count4A=function(){
-  if(count4>1){
-    count4=count4-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result4").innerText=count4;
-}
-
-var count4B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count4=count4+1;
-  document.getElementById("result4").innerText=count4;
-}
-
-function resetcount4(){
-  count4=1;
-  document.getElementById("result4").innerText=count4;
-
-}
-
-var count5=1;
-var count5A=function(){
-  if(count5>1){
-    count5=count5-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result5").innerText=count5;
-}
-
-var count5B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count5=count5+1;
-  document.getElementById("result5").innerText=count5;
-}
-
-function resetcount5(){
-  count5=1;
-  document.getElementById("result5").innerText=count5;
-
-}
-
-var count6=1;
-var count6A=function(){
-  if(count6>1){
-    count6=count6-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result6").innerText=count6;
-}
-
-var count6B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count6=count6+1;
-  document.getElementById("result6").innerText=count6;
-}
-
-function resetcount6(){
-  count6=1;
-  document.getElementById("result6").innerText=count6;
-
-}
-
-var count7=1;
-var count7A=function(){
-  if(count7>1){
-    count7=count7-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result7").innerText=count7;
-}
-
-var count7B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count7=count7+1;
-  document.getElementById("result7").innerText=count7;
-}
-
-function resetcount7(){
-  count7=1;
-  document.getElementById("result7").innerText=count7;
-
-}
-
-var count8=1;
-var count8A=function(){
-  if(count8>1){
-    count8=count8-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result8").innerText=count8;
-}
-
-var count8B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count8=count8+1;
-  document.getElementById("result8").innerText=count8;
-}
-
-function resetcount8(){
-  count8=1;
-  document.getElementById("result8").innerText=count8;
-
-}
-
-var count9=1;
-var count9A=function(){
-  if(count9>1){
-    count9=count9-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result9").innerText=count9;
-}
-
-var count9B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count9=count9+1;
-  document.getElementById("result9").innerText=count9;
-}
-
-function resetcount9(){
-  count9=1;
-  document.getElementById("result9").innerText=count9;
-
-}
-
-
-var count10=1;
-var count10A=function(){
-  if(count10>1){
-    count10=count10-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-  document.getElementById("result10").innerText=count10;
-}
-
-var count10B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count10=count10+1;
-  document.getElementById("result10").innerText=count10;
-}
-
-function resetcount10(){
-  count10=1;
-  document.getElementById("result10").innerText=count10;
-
-}
-
-var count11=1;
-var count11A=function(){
-  if(count11>1){
-    count11=count11-1;
-    totalnum=totalnum-1;
-  }
-  totalnum=totalnum-1;
-  document.getElementById("total").innerText=totalnum;
-  count11=count11-1;
-  if(count11<1){count11=1}
-  document.getElementById("result11").innerText=count11;
-}
-
-var count11B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count11=count11+1;
-  document.getElementById("result11").innerText=count11;
-}
-
-function resetcount11(){
-  count11=1;
-  document.getElementById("result11").innerText=count11;
-
-}
-
-var count12=1;
-var count12A=function(){
-  if(count12>1){
-    count12=count12-1;
-    totalnum=totalnum-1;
-  }
-  document.getElementById("total").innerText=totalnum;
-    document.getElementById("result12").innerText=count12;
-}
-
-var count12B=function(){
-  totalnum=totalnum+1;
-  document.getElementById("total").innerText=totalnum;
-  count12=count12+1;
-  document.getElementById("result12").innerText=count12;
-}
-
-function resetcount12(){
-  count12=1;
-  document.getElementById("result12").innerText=count12;
-
-}
-
-var clicked1=false;
-var clicked2=false;
-var clicked3=false;
-var clicked4=false;
-var clicked5=false;
-var clicked6=false;
-var clicked7=false;
-var clicked8=false;
-var clicked9=false;
-var clicked10=false;
-var clicked11=false;
-var clicked12=false;
-
-function resetClicked() {
-  clicked1 = false;
-  clicked2 = false;
-  clicked3 = false;
-  clicked4 = false;
-  clicked5 = false;
-  clicked6 = false;
-  clicked7 = false;
-  clicked8 = false;
-  clicked9 = false;
-  clicked10 = false;
-  clicked11= false;
-  clicked12 = false;
-
-}
-
-// 색상 변경 및 버튼 클릭 시 보이도록  
- const numbutton=document.querySelector("#numbutton")
-  const btnbeige1=document.getElementById("menu1");
-  const btnbeige2=document.getElementById("menu5");
-  const btnbeige3=document.getElementById("menu9")
-
-
-function handleClick1(e) {
-  if (!clicked1) {
-    menu1.style.backgroundColor = "#FFF6EA";
-    btnvisible1();
-    totalnum++;
-    document.getElementById("total").innerText = totalnum;
-    clicked1 = true;
+  if (numButton.style.display === "" || numButton.style.display === "none") {
+    numButton.style.display = "flex";
+    menu.style.backgroundColor = "#FFF6EA";
+    total++;
+    totalElement.textContent = total;
   }
 }
 
-btnbeige1.addEventListener("click", handleClick1);
-  
-function handleClick2(e) {
-  if (!clicked2) {
-    menu5.style.backgroundColor = "#FFF6EA";
-    btnvisible5();
-    totalnum++;
-    document.getElementById("total").innerText = totalnum;
-    clicked2 = true;
+function countA(button) {
+  var numButton = button.parentElement;
+  var countElement = numButton.querySelector("#result");
+  var currentCount = parseInt(numButton.dataset.count);
+
+  if (currentCount > 1) {
+    numButton.dataset.count = currentCount - 1;
+    countElement.textContent = numButton.dataset.count;
+    total--;
+    totalElement.textContent = total;
   }
 }
 
-btnbeige2.addEventListener("click", handleClick2);
+function countB(button) {
+  var numButton = button.parentElement;
+  var countElement = numButton.querySelector("#result");
+  var currentCount = parseInt(numButton.dataset.count);
 
-
-function handleClick3(e) {
-  if (!clicked3) {
-    menu9.style.backgroundColor = "#FFF6EA";
-    btnvisible9();
-    totalnum++;
-    document.getElementById("total").innerText = totalnum;
-    clicked3 = true;
-  }
+  numButton.dataset.count = currentCount + 1;
+  countElement.textContent = numButton.dataset.count;
+  total++;
+  totalElement.textContent = total;
 }
 
-btnbeige3.addEventListener("click", handleClick3);
 
-  
- 
-   const btnpink1=document.getElementById("menu2");
-   const btnpink2=document.getElementById("menu6");
-   const btnpink3=document.getElementById("menu10");
+function resetCounts() {
+  var numButtons = document.querySelectorAll("#numbutton");
+  var menus = document.querySelectorAll("#menu");
 
-
-   function handleClick4(e) {
-    if (!clicked4) {
-      menu2.style.backgroundColor="#f8e3e3";
-    btnvisible2();
-    totalnum++;
-    document.getElementById("total").innerText=totalnum;
-      clicked4 = true;
-    }
+  for (var i = 0; i < numButtons.length; i++) {
+    var numButton = numButtons[i];
+    numButton.dataset.count = 1;
+    numButton.querySelector("#result").textContent = 1;
+    numButton.classList.remove("active");
+    numButton.style.display = "none";
   }
-  btnpink1.addEventListener("click", handleClick4);
 
-
-  function handleClick5(e) {
-    if (!clicked5) {
-      menu6.style.backgroundColor="#f8e3e3";
-        btnvisible6();
-        totalnum++;
-        document.getElementById("total").innerText=totalnum;
-      clicked5 = true;
-    }
+  for (var i = 0; i < menus.length; i++) {
+    var menu = menus[i];
+    var numButton = menu.querySelector("#numbutton");
+    numButton.classList.remove("active");
+    menu.style.backgroundColor = "white";
   }
-  btnpink2.addEventListener("click", handleClick5);
 
-  function handleClick6(e) {
-    if (!clicked6) {
-      menu10.style.backgroundColor="#f8e3e3";
-        btnvisible10();
-        totalnum++;
-        document.getElementById("total").innerText=totalnum;
-      clicked6 = true;
-    }
-  }
-  btnpink3.addEventListener("click", handleClick6);
-
-   const btnblue1=document.getElementById("menu3");
-   const btnblue2=document.getElementById("menu7");
-   const btnblue3=document.getElementById("menu11");
-
-   function handleClick7(e) {
-    if (!clicked7) {
-      menu3.style.backgroundColor="#E7F3FF";
-      btnvisible3();
-      totalnum++;
-      document.getElementById("total").innerText=totalnum;
-      clicked7 = true;
-    }
-  }
-  btnblue1.addEventListener("click", handleClick7);
-
-  function handleClick8(e) {
-    if (!clicked8) {
-      menu7.style.backgroundColor="#E7F3FF";
-    btnvisible7();
-    totalnum++;
-    document.getElementById("total").innerText=totalnum;
-      clicked8 = true;
-    }
-  }
-  btnblue2.addEventListener("click", handleClick8);
-
-
-  function handleClick9(e) {
-    if (!clicked9) {
-      menu11.style.backgroundColor="#E7F3FF";
-    btnvisible11();
-    totalnum++;
-    document.getElementById("total").innerText=totalnum;
-      clicked9 = true;
-    }
-  }
-  btnblue3.addEventListener("click", handleClick9);
-
-   const btnyellow1=document.getElementById("menu4");
-   const btnyellow2=document.getElementById("menu8");
-   const btnyellow3=document.getElementById("menu12");
-
-   function handleClick10(e) {
-    if (!clicked10) {
-      menu4.style.backgroundColor="#f6e2d0";
-    btnvisible4();
-    totalnum++;
-    document.getElementById("total").innerText=totalnum;
-      clicked10 = true;
-    }
-  }
-  btnyellow1.addEventListener("click", handleClick10);
-
-  function handleClick11(e) {
-    if (!clicked11) {
-      menu8.style.backgroundColor="#f6e2d0";
-      btnvisible8();
-      totalnum++;
-      document.getElementById("total").innerText=totalnum;
-      clicked11 = true;
-    }
-  }
-  btnyellow2.addEventListener("click", handleClick11);
-
-
-  function handleClick12(e) {
-    if (!clicked12) {
-      menu12.style.backgroundColor="#f6e2d0";
-      btnvisible12();
-      totalnum++;
-      document.getElementById("total").innerText=totalnum;
-      clicked12 = true;
-    }
-  }
-  btnyellow3.addEventListener("click", handleClick12);
-
-
-   function btnvisible1(){
-    numbutton1.setAttribute('class','active')
-
-}function btnvisible2(){
-    numbutton2.setAttribute('class','active')
-
-}function btnvisible3(){
-    numbutton3.setAttribute('class','active')
-
-}function btnvisible4(){
-    numbutton4.setAttribute('class','active')
-
-}function btnvisible5(){
-    numbutton5.setAttribute('class','active')
-
-}function btnvisible6(){
-    numbutton6.setAttribute('class','active')
-
-}function btnvisible7(){
-    numbutton7.setAttribute('class','active')
-
-}function btnvisible8(){
-    numbutton8.setAttribute('class','active')
-
-}function btnvisible9(){
-    numbutton9.setAttribute('class','active')
-
-}function btnvisible10(){
-    numbutton10.setAttribute('class','active')
-
-}function btnvisible11(){
-    numbutton11.setAttribute('class','active')
-
-}function btnvisible12(){
-    numbutton12.setAttribute('class','active')
-
+  total = 0;
+  totalElement.textContent = total;
 }
-
-function cancelbtn(){
-  numbutton1.removeAttribute('class','active');
-  numbutton2.removeAttribute('class','active');
-  numbutton3.removeAttribute('class','active');
-  numbutton4.removeAttribute('class','active');
-  numbutton5.removeAttribute('class','active');
-  numbutton6.removeAttribute('class','active');
-  numbutton7.removeAttribute('class','active');
-  numbutton8.removeAttribute('class','active');
-  numbutton9.removeAttribute('class','active');
-  numbutton10.removeAttribute('class','active');
-  numbutton11.removeAttribute('class','active');
-  numbutton12.removeAttribute('class','active');
-  menu1.style.backgroundColor="white";
-  menu2.style.backgroundColor="white";
-  menu3.style.backgroundColor="white";
-  menu4.style.backgroundColor="white";
-  menu5.style.backgroundColor="white";
-  menu6.style.backgroundColor="white";
-  menu7.style.backgroundColor="white";
-  menu8.style.backgroundColor="white";
-  menu9.style.backgroundColor="white";
-  menu10.style.backgroundColor="white";
-  menu11.style.backgroundColor="white";
-  menu12.style.backgroundColor="white";
- 
-}
-
-function cancel(){
-  totalnums();
-  cancelbtn();
-  resetcount1();
-  resetcount2();
-  resetcount3();
-  resetcount4();
-  resetcount5();
-  resetcount6();
-  resetcount7();
-  resetcount8();
-  resetcount9();
-  resetcount10();
-  resetcount11();
-  resetcount12();
-  
-  document.getElementById("total").innerText=totalnum;
-  resetClicked();
-
-}
-
